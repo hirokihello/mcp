4b9a033fc7762438f9c5aeb3009a816aac7e7904
hirokihello <iammyeye1@gmail.com>
2019-09-15 22:57:24 +0900

    timeoutの時間の延長

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'docker'
 require 'sinatra/reloader'
 require 'pry-byebug'
 
+set :server_settings, :timeout => 60
+
 get '/' do
   image = Docker::Image.create('fromImage' => 'ruby:2.6.3')
   image.tag('repo' => 'ruby-base-hirokihello', 'tag' => 'latest', force: true)
@@ -16,16 +18,12 @@ get '/' do
     'logs' => true
   )
   container.start
-  binding.pry
   puts container.exec(['apt', 'update'], stdout: true, tty: true)
-  puts container.exec(['ls', '-l'], stdout: true, tty: true)
-  container.exec(['apt', 'install', 'git'], stdout: true, tty: true)
-  container.exec(['ssh', '-T', 'git@git.com'], stdout: true, tty: true)
-  container.exec(['curl', 'https://example.com'], stdout: true, tty: true)
-  puts "pwd command"
+  puts container.exec(['apt', 'install', 'git'], stdout: true, tty: true)
   puts container.exec(['git', 'clone', 'https://github.com/hirokihello/rails-realworld-example-app.git'], stdout: true, tty: true)
+  puts container.exec(['ls', '-l'], stdout: true, tty: true)
   command = ["/bin/bash", "-c", "echo -n \"I'm a TTY!\""]
   puts container.exec(command, tty: true)
-  container.kill
+  container.stop
   "hello"
 end
