57289d1989a348f8086131ef5d3a86a1ec248420
hirokihello <iammyeye1@gmail.com>
2019-09-15 23:01:30 +0900

    mysqlイメージを追加

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ set :server_settings, :timeout => 60
 get '/' do
   image = Docker::Image.create('fromImage' => 'ruby:2.6.3')
   image.tag('repo' => 'ruby-base-hirokihello', 'tag' => 'latest', force: true)
-  container = Docker::Container.create(
+  app_container = Docker::Container.create(
     'Cmd' => ['/bin/bash'],
     'Image' => 'ruby-base-hirokihello:latest',
     'OpenStdin' => true,
@@ -17,13 +17,16 @@ get '/' do
     'tty' => true,
     'logs' => true
   )
-  container.start
-  puts container.exec(['apt', 'update'], stdout: true, tty: true)
-  puts container.exec(['apt', 'install', 'git'], stdout: true, tty: true)
-  puts container.exec(['git', 'clone', 'https://github.com/hirokihello/rails-realworld-example-app.git'], stdout: true, tty: true)
-  puts container.exec(['ls', '-l'], stdout: true, tty: true)
+  db_container = Docker::Container.create(
+    'Image' => 'mysql:5.7'
+  )
+  app_container.start
+  puts app_container.exec(['apt', 'update'], stdout: true, tty: true)
+  puts app_container.exec(['apt', 'install', 'git'], stdout: true, tty: true)
+  puts app_container.exec(['git', 'clone', 'https://github.com/hirokihello/rails-realworld-example-app.git'], stdout: true, tty: true)
+  puts app_container.exec(['ls', '-l'], stdout: true, tty: true)
   command = ["/bin/bash", "-c", "echo -n \"I'm a TTY!\""]
-  puts container.exec(command, tty: true)
-  container.stop
+  puts app_container.exec(command, tty: true)
+  app_container.stop
   "hello"
 end
